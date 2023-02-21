970ad3f55b9b314316a48f3213d72968652719e5
Dycov <sndirangu3@gmail.com>
2023-02-21 11:22:36 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,19 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-	char *sh = "Main";
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
-
+	_putchar('_')
+	_putchar('p')
+	_putchar('u')
+	_putchar('t')
+	_putchar('c')
+	_putchar('h')
+	_putchar('a')
+	_putchar('r')
+	_putchar('\n')
 	return (0);
 }
