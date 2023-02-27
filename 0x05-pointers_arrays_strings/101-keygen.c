43f722ac7eff04eaf66a863507d2a42c9d01da4a
Dycov <sndirangu3@gmail.com>
2023-02-27 11:09:11 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,25 +11,16 @@
 
 int main(void)
 {
-	int i, sum, n;
-	int pass[100];
-
-	sum = 0;
+	int sum;
+	char c;
 
 	srand(time(NULL));
-	for (i = 0; i < 100; i++);
+	while (sum <= 2645)
 	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78);
-		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
 	}
-
+	putchar(2772 - sum);
 	return (0);
 }
