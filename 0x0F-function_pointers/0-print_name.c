8605ad9f503ee4b5bfdccbfed34e1bca1fca205e
Dycov <sndirangu3@gmail.com>
2023-03-22 02:30:55 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+int main()
+void print_name(char *name);
+{
+	printf("My name is %s\n", name);
+
+	return(0)
+}
